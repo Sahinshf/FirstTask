bd7c1782453b3c09fcfb91a0d70da7147f804795
Sahinshf <shahinrsha@code.edu.az>
2023-07-20 00:36:34 +0400

    Task 3->8 Added

---
+++ Task/Program.cs
@@ -0,0 +1,79 @@
+﻿namespace Task;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+
+        //Task 3
+        //int num1 = int.Parse(Console.ReadLine());
+        //int num2 = int.Parse(Console.ReadLine());
+        //int start;
+        //int end;
+
+        //if (num1>=num2)
+        //{
+        //    start = num2; end = num1;    
+        //}
+        //else
+        //{
+        //    end = num2; start = num1;
+        //}
+
+        //for (int i = start; i <= end; i++)
+        //{
+        //    Console.WriteLine(i);
+        //}
+
+
+        //Task 4
+        //int n = int.Parse(Console.ReadLine());
+        //int m = int.Parse(Console.ReadLine());
+        //int pow=1;
+
+        //for (int i = 0; i < n; i++)
+        //{
+        //    pow = pow * m;
+        //}
+        //Console.WriteLine(pow);
+
+
+        //Task 5
+        //int number = int.Parse(Console.ReadLine());
+        //int count = 0;
+        //while (number > 0)
+        //{
+        //    number /= 10;
+        //    count++;
+        //}
+
+        //Console.WriteLine(count);
+
+
+        //Task 6
+        //string[] arr = { "Shahin", "Fikret", "Asiman", "Tural", "Huseyn", "Eli" };
+        //for (int i = 0; i < arr.Length-1; i++)
+        //{
+        //    if (arr[i].Length>4)
+        //    {
+        //        Console.WriteLine(arr[i]);
+        //    }
+        //}
+
+
+        //Task 7
+        //int num1 = Convert.ToInt32(Console.ReadLine());
+        //int num2 = Convert.ToInt32(Console.ReadLine());
+        //int count = Convert.ToInt32(Console.ReadLine());
+
+        //Console.Write($"{num1} {num2} ");
+        //for (int i = 2; i < count; i++)
+        //{
+        //    int next = num1 + num2;
+        //    Console.Write(next + " ");
+
+        //    num1 = num2;
+        //    num2 = next;
+        //}
+    }
+}
